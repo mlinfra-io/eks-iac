5ed42659ecbeb6fb8e6cc058b7ba9f209b25f485
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2026-01-16 02:34:11 +0100

    Added karpenter helm chart
    Signed-off-by: Ali Abbas Jaffri <ali.abbasjaffri@live.com>

--- modules/aws/eks/karpenter.tf
+++ modules/aws/eks/karpenter.tf
@@ -1,4 +1,4 @@
-module "eks_karpenter" {
+module "karpenter_dependencies" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
   version = "~> 21.12.0"
 
@@ -8,11 +8,35 @@ module "eks_karpenter" {
 
   # Name needs to match role name passed to the EC2NodeClass
   node_iam_role_use_name_prefix   = false
-  node_iam_role_name              = var.cluster_name
+  node_iam_role_name              = module.eks.cluster_name
   create_pod_identity_association = true
 
   # Used to attach additional IAM policies to the Karpenter node IAM role
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
+}
+
+resource "helm_release" "karpenter" {
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  namespace  = "kube-system"
+  version    = "1.8.5"
+
+  set = [{
+    name  = "settings.clusterName"
+    value = module.eks.cluster_name
+    }, {
+    name  = "settings.interruptionQueue"
+    value = module.eks.cluster_name
+    }, {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter_dependencies.iam_role_arn
+    }
+  ]
+
+  depends_on = [
+    module.karpenter_dependencies
+  ]
 }
